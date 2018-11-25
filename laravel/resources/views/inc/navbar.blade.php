0be3786812920485935cb5ff18d0a2f65e760840
Alexander <a.georgiev@student.fontys.nl>
2018-11-25 20:15:23 +0100

    styling navbar

--- laravel/resources/views/inc/navbar.blade.php
+++ laravel/resources/views/inc/navbar.blade.php
@@ -1,9 +1,9 @@
-<nav class="navbar navbar-dark bg-dark">
-    <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Blogsters') }}
-        </a>
-        <ul class="nav justify-content-center">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="{{ url('/') }}">
+        {{ config('app.name', 'Blogsters') }}
+    </a>
+    <div class="collapse navbar-collapse justify-content-center">
+        <ul class="nav">
             <li><a class="nav-link" href="{{ url('/') }}">Home</a></li>
             <li><a class="nav-link" href="{{ url('/about') }}">About</a></li>
         </ul>
--- laravel/resources/views/pages/index.blade.php
+++ laravel/resources/views/pages/index.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="jumbotron text-center">
     <h2>{{$title}}</h2>
+</div>
 @endsection
