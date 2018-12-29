43da7fe9ca4e406a2b7da0bdc5e0eb174decd77e
Dimitar Dyakov <dimitar.dyakov98@gmail.com>
2018-12-29 15:45:00 +0100

    prettyfy excel export

--- laravel/app/Exports/UsersExport.php
+++ laravel/app/Exports/UsersExport.php
@@ -4,14 +4,41 @@ namespace App\Exports;
 
 use App\User;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
 
-class UsersExport implements FromCollection
+class UsersExport implements 
+    FromCollection, WithHeadings, ShouldAutoSize, WithEvents
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
     public function collection()
     {
-        return User::all();
+        $users = User::all('id', 'name', 'is_admin', 'email', 'created_at');
+
+        foreach ($users as $user) {
+            $user['is_admin'] = $user['is_admin'] ? 'Yes' : 'No';
+        }
+
+        return $users;
+    }
+
+    public function headings(): array {
+        return [
+            '#',
+            'Name',
+            'Admin',
+            'Email',
+            'Created at'
+        ];
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function(AfterSheet $event) {
+                $event->sheet->getDelegate()->getStyle('A1:E1')->getFont()->setSize(14);
+            },
+        ];
     }
 }
